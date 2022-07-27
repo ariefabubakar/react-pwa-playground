9f6405497241698e18bc9369371e3ab4e96e9afd
Arief <arief.ab@live.com>
2022-07-27 14:17:28 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import { useState, useEffect } from "react";
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 import axios from "axios";
 import preval from "preval.macro";
 import moment from "moment";
+import Reports from "./Reports";
 
 axios.defaults.baseURL = "https://pwa-restapi-scs.herokuapp.com";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+// axios.defaults.baseURL = "http://localhost:5000";
 
 function App() {
-  const [message, setMessage] = useState("");
   const version = moment(preval`module.exports = new Date().getTime();`).format(
     "YY.MM.DD.HHmm"
   );
 
-  useEffect(() => {
-    axios.get('/api/test').then(function (response) {
-      // handle success
-      setMessage(response.data.message);
-    });
-  });
-
   return (
-    <div>
-      <div>
-    {message}
-    </div>
-    <footer>
-      Versi {version}
-      </footer>
+    <div className="App">
+      <Reports />
+      <hr class="solid" />
+      <footer style={{ marginTop: 10 }}>Versi {version}</footer>
     </div>
   );
 }
